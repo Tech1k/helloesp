47b794fd90570de48da88a09c4301128a2442759
Tech1k <kristian@beyondcoin.io>
2022-07-05 19:33:32 -0600

    Add chatbox

--- helloesp.ino
+++ helloesp.ino
@@ -240,6 +240,9 @@ String HTML = R"rawliteral(
                 padding-bottom: 15px;
             }
         </style>
+
+        <!-- Chatbro chat box -->
+        <script id="chatBroEmbedCode">/* Chatbro Widget Embed Code Start */function ChatbroLoader(chats,async){async=!1!==async;var params={embedChatsParameters:chats instanceof Array?chats:[chats],lang:navigator.language||navigator.userLanguage,needLoadCode:'undefined'==typeof Chatbro,embedParamsVersion:localStorage.embedParamsVersion,chatbroScriptVersion:localStorage.chatbroScriptVersion},xhr=new XMLHttpRequest;xhr.withCredentials=!0,xhr.onload=function(){eval(xhr.responseText)},xhr.onerror=function(){console.error('Chatbro loading error')},xhr.open('GET','//www.chatbro.com/embed.js?'+btoa(unescape(encodeURIComponent(JSON.stringify(params)))),async),xhr.send()}/* Chatbro Widget Embed Code End */ChatbroLoader({encodedChatId: '68VRf'});</script>
     </head>
     <body>
         <div class='HMP'>
