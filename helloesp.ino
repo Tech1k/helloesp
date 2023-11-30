071d3d61014c69eb61027df29e9aa7a5c8e0b1d3
Tech1k <kristian@kk.dev>
2023-11-30 11:00:54 -0700

    GPL -> MIT

--- helloesp.ino
+++ helloesp.ino
@@ -1,21 +1,6 @@
-/*
-  helloesp.ino - Code for the ESP webserver
-
-  Copyright (C) 2022-2023 Kristian Kramer. All rights reserved.
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+// Copyright (c) 2022-2023 Kristian Kramer
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or https://opensource.org/license/mit/
 
 #include "ESPAsyncWebServer.h"
 #include "uptime_formatter.h"
